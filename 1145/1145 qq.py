6a0d3fcf6c5122dd4f360cbb188e33ceb084efee
SliDeR <33261295+Palmson@users.noreply.github.com>
2023-06-03 17:52:09 +0300

    Update 1145 qq.py

--- 1145/1145 qq.py
+++ 1145/1145 qq.py
@@ -38,9 +38,3 @@ for i in range(n):
             laby[i][j] = 1
 
 shortest_rope(laby)
-#...The next lines describe the labyrinth. The ith line of the next m lines contains n characters. Each character is either "#" or ".", with "#" indicating a forbidden cell, and "." indicating a free cell (i = 1, 2, ..., m)...
-#But in sample input the last character in the 1st line of labyrinth is space.
-#The problem asks you to find the diameter of a tree.
-#You can speed up BFS from a source to a target by doing a bi-directional search.
-
-#A bi-directional search is basically doing a BFS from the source and from the target at the same time, one step from each - until the two fronts meet each other.
